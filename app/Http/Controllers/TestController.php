9737c5073467efb1f476ed33eb186a4890649dbd
pang-xin <1660696636@qq.com>
2019-12-31 02:01:12 +0000

    api

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,26 +8,72 @@ class TestController extends Controller
 {
     public function alipay()
     {
-        $url="https://openapi.alipay.com/gateway.do"; //支付宝网关
+        $url = "https://openapi.alipay.com/gateway.do"; //支付宝网关
 
-        $app_id="2016101400681519";
-        $method="alipay.trade.page.pay";
-        $charset="utf8";
-        $sing_type="RSA2";
-        $sing='';
-        $timestamp=date("Y-m-d H:i:s");
-        $version="1.0";
+        $app_id = "2016101400681519";
+        $method = "alipay.trade.page.pay";
+        $charset = "utf8";
+        $sing_type = "RSA2";
+        $sing = '';
+        $timestamp = date("Y-m-d H:i:s");
+        $version = "1.0";
         $return_url = 'http://1905api.comcto.com/test/alipay/return';       // 支付宝同步通知
         $notify_url = 'http://1905api.comcto.com/test/alipay/notify';        // 支付宝异步通知地址
         $biz_content = '';
 
         // 请求参数
-        $out_trade_no = time() . rand(1111,9999);       //商户订单号
+        $out_trade_no = time() . rand(1111, 9999);       //商户订单号
         $product_code = 'FAST_INSTANT_TRADE_PAY';
         $total_amount = 0.01;
         $subject = '测试订单' . $out_trade_no;
 
-        $a=1;
+        $request_param = [
+            'out_trade_no' => $out_trade_no,
+            'product_code' => $product_code,
+            'total_amount' => $total_amount,
+            'subject' => $subject
+        ];
+        $param = [
+            'app_id' => $app_id,
+            'method' => $method,
+            'charset' => $charset,
+            'sign_type' => $sing_type,
+            'timestamp' => $timestamp,
+            'version' => $version,
+            'notify_url' => $notify_url,
+            'return_url' => $return_url,
+            'biz_content' => json_encode($request_param)
+        ];
+        //echo '<pre>';print_r($param);echo '</pre>';
+        // 字典序排序
+        ksort($param);
+        //echo '<pre>';print_r($param);echo '</pre>';
+        // 2 拼接 key1=value1&key2=value2...
+        $str = "";
+        foreach ($param as $k => $v) {
+            $str .= $k . '=' . $v . '&';
+        }
+        //echo 'str: '.$str;echo '</br>';
+        $str = rtrim($str, '&');
+        //echo 'str: '.$str;echo '</br>';echo '<hr>';
+        // 3 计算签名   https://docs.open.alipay.com/291/106118
+        $key = storage_path('keys/app_priv');
+        $priKey = file_get_contents($key);
+        $res = openssl_get_privatekey($priKey);
+        //var_dump($res);echo '</br>';
+        openssl_sign($str, $sign, $res, OPENSSL_ALGO_SHA256);
+        $sign = base64_encode($sign);
+        $param['sign'] = $sign;
+        // 4 urlencode
+        $param_str = '?';
+        foreach ($param as $k => $v) {
+            $param_str .= $k . '=' . urlencode($v) . '&';
+        }
+        $param_str = rtrim($param_str, '&');
+        $url = $url . $param_str;
+        //发送GET请求
+        //echo $url;die;
+        header("Location:" . $url);
 
     }
 }
