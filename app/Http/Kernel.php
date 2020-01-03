415d7e2d0290ee68c139ee1a86cf10d4e80bb57f
pang-xin <1660696636@qq.com>
2020-01-03 06:02:40 +0000

    Api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'ApiHeader'=>\App\Http\Middleware\ApiHeader::class,
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::prefix('/')->middleware('ApiHeader')->group(function () {
+    Route::post('/a', 'Api\ApiController@a');
+});
+Route::get('/brush','Api\ApiController@brush');
+Route::post('/index', 'Api\ApiController@index');
+Route::post('/create', 'Api\ApiController@create');
